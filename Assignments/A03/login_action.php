c93a3720e82a7faae52a249fccb19375a45dee29
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2021-04-02 11:33:26 +0300

    Updated Code

--- Assignments/A03/login_action.php
+++ Assignments/A03/login_action.php
@@ -5,19 +5,24 @@
 
         $username = sanitize_sql($connection, $_POST['username']);
 
-        $pass_query = "SELECT `password` FROM `user_accounts` WHERE `user_name` = '$username'";
-        if($result = mysqli_query($connection, $pass_query)){
+        $query = "SELECT `password` FROM `user_accounts` WHERE `user_name` = '$username'";
+
+        if($result = mysqli_query($connection, $query)){
             if(mysqli_num_rows($result) > 0){
                 $result = mysqli_fetch_array($result);
                 if(password_verify($_POST['password'], $result[0])){
                     echo "Welcome";
+                }else{
+                    die("Wrong username or password");
                 }
             }else{
-                echo "Wrong username or password";
+                die("Wrong username or password");
             }
+        }else{
+            die("An error has occurred");
         }
     }else{
-        echo "Please fill all fields";
+        die("Please fill all fields");
     }
      
     mysqli_close($connection);
--- Assignments/A03/registration_action.php
+++ Assignments/A03/registration_action.php
@@ -8,14 +8,19 @@
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $stmt = $connection->prepare("INSERT INTO `user_accounts`(`user_name`, `email`, `password`) VALUES (?,?,?)");
         $stmt->bind_param('sss', $username, $email, $password);
-        $stmt->execute();
-        $stmt->close();
-        mysqli_close($connection);
-        header('Location: login.html');
-        exit();
-
+        if(false === $stmt->execute){
+            echo "An error has occurred";
+            $stmt->close();
+            exit();
+        }else{
+            $stmt->execute();
+            $stmt->close();
+            mysqli_close($connection);
+            header('Location: login.html');
+            exit();
+        }
     }else{
         echo "Please fill all fields";
     }
-       
+
 ?>
