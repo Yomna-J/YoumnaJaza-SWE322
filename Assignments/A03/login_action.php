e804484f820f65109dafdde174087357f5675ab6
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2021-04-08 22:28:42 +0300

    Edited prepared statment

--- Assignments/A03/login_action.php
+++ Assignments/A03/login_action.php
@@ -3,39 +3,29 @@
 
     if(isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])){
 
-        $username = sanitize_sql($connection, $_POST['username']);
+        $username = $_POST['username'];
 
-        $query = "SELECT `password` FROM `user_accounts` WHERE `user_name` = '$username'";
-
-        if($result = mysqli_query($connection, $query)){
-            if(mysqli_num_rows($result) > 0){
-                $result = mysqli_fetch_array($result);
-                if(password_verify($_POST['password'], $result[0])){
+        $stmt = $connection->prepare("SELECT `password` FROM `user_accounts` WHERE `user_name` = ? ");
+        $stmt->bind_param('s', $username);
+        if(false === $stmt->execute()){
+            echo "An error has occurred";
+            $stmt->close();
+            exit();
+        }else{
+            $result = $stmt->get_result();
+            while ($row = $result->fetch_assoc()) {
+                if(password_verify($_POST['password'], $row['password'])){
                     echo "Welcome";
                 }else{
                     die("Wrong username or password");
                 }
-            }else{
-                die("Wrong username or password");
             }
-        }else{
-            die("An error has occurred");
+            $stmt->close();
+            mysqli_close($connection);
+            exit();
         }
     }else{
         die("Please fill all fields");
-    }
-     
-    mysqli_close($connection);
-
-    function test_input($data){
-        $data = stripslashes($data); 
-        $data = trim($data); 
-        return $data;
-    }
-    function sanitize_sql($conn, $data) {
-        $data = mysqli_real_escape_string($conn, $data);
-        $data = test_input($data);
-        return $data;
-    }
-
+    }    
+       
 ?>
