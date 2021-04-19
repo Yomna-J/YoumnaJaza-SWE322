1535e8bfcae3858dfe540321407d505c66abe553
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2021-04-20 02:27:40 +0300

    Added regesiterstation action

---
+++ Project/www/Regsiteration_action.php
@@ -0,0 +1,23 @@
+<?php
+
+    include("config/config.php");
+    include("src/php/sanitizeInput.php");
+
+    if(isset($_POST['submit'])){
+        $username = fix_string($_POST['username']);
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        if(validate_form($email, $username, $password) == true){
+            $stmt = $connection->prepare("INSERT INTO `user_account`(`username`, `email`, `password`) VALUES (?,?,?)");
+            $stmt->bind_param('sss', $username, $email, $password);
+           
+        }        
+
+    }
+
+
+
+?>
