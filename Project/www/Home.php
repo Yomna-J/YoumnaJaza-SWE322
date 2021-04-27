9b17d6283cb816ad530e20dec332bdf71188dbaa
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2021-04-28 02:12:45 +0300

    Fixed bugs
    - Fixed an issue of not accessing the DB
    - Fixed an classes id and array

--- Project/www/Home.php
+++ Project/www/Home.php
@@ -5,56 +5,45 @@
 
     #TO DO: check allowed access
 
-
     include("../config/db_login.php");
 
-    $query="SELECT * FROM `classes` ";
-    $result= mysqli_query($connection, $query);  
+    $query = "SELECT * FROM `classes` ";
+    $result = mysqli_query($connection, $query);
 
-    
     if ($result = mysqli_query($connection, $query)) {
-        if(mysqli_num_rows($result) > 0){ 
+        if (mysqli_num_rows($result) > 0) {
             $result = mysqli_query($connection, $query);
             echo "<form action='' method='post'>";
             $num_all_trainees = array();
             while ($result_row = mysqli_fetch_row($result)) { # Loop over all classes
-                if($result_row[4] < $result_row[5]){ # if it doesn't exceed max
-                    if(date('Y-m-d') <= $result_row[3]){ # checks if the class date has passed
-                        echo"$result_row[1] <br>";
-
+                if ($result_row[4] < $result_row[5]) { # if it doesn't exceed max
+                    if (date('Y-m-d') <= $result_row[3]) { # checks if the class date has passed
                         echo "<input type='checkbox' name='classes[]' 
-                        value='$result_row[0]'/> $result_row[1] <br />";
-                        $num_all_trainees += array($result_row[0], $result_row[4]);// array of all classes id , number of trainees 
-                    }           
-                }
-
-            }
-            echo "<input type='submit' name='confirm' value='Confirm' />
-                  </form>";
+                            value='$result_row[0]'/> $result_row[1] <br />";
+                            
+                        $num_all_trainees[$result_row[0]] = $result_row[4]; // array of all classes id , number of trainees                    
+                    }
+                } 
+            }  
+            echo "<input type='submit' name='confirm' value='Confirm' /> </form>";
         }
     }
-    if ( isset($_POST['confirm'])){
-    $classes = $_POST['classes'];// id{1,2,3,4}
-    if(empty($classes)) 
-    {
-   echo("You didn't select any classes.");
-    } 
-   else 
-  {
-    $num_classes = count($classes);
-    
-   for($i=0; $i < $num_classes; $i++)
-    {
-    $new_num_trainees = $num_all_trainees[$classes[$i]];
-      $query = 'UPDATE classes SET number_of_trainees = $new_num_trainees WHERE class_id =$classes[$i]';
-       $result= mysqli_query($connection, $query);  
 
-    if (!$result = mysqli_query($connection, $query)){
-        die ('An error occured');
-    }
-       
-    }
-  }
-    
+    if (isset($_POST['confirm'])) { #ToDO: doesn't check
+
+        $classes = $_POST['classes']; // id{1,2,3,4} 
+        if (empty($classes)) {
+            die ("You didn't select any classes.");
+        } else {
+            $num_classes = count($classes); # num of user choices length of array
+            for ($i = 0; $i < $num_classes; $i++) {
+                $new_num_trainees = $num_all_trainees[$classes[$i]]+1;
+                $query = "UPDATE `classes` SET `number_of_trainees`= '$new_num_trainees' WHERE class_id = '$classes[$i]'";
+                $result = mysqli_query($connection, $query);
+                if (!$result = mysqli_query($connection, $query)) {
+                    die('An error occurred');
+                }
+            }
+        }
     }
 ?>
