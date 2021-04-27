6843d1840a1e62f9c64acb1f33f018c0a22daa4a
halahAllabun <61107345+halahAllabun@users.noreply.github.com>
2021-04-28 00:38:06 +0300

    Update Home.php

--- Project/www/Home.php
+++ Project/www/Home.php
@@ -11,19 +11,22 @@
     $query="SELECT * FROM `classes` ";
     $result= mysqli_query($connection, $query);  
 
-
+    
     if ($result = mysqli_query($connection, $query)) {
         if(mysqli_num_rows($result) > 0){ 
             $result = mysqli_query($connection, $query);
+            echo "<form action='' method='post'>";
             while ($result_row = mysqli_fetch_row($result)) { # Loop over all classes
                 if($result_row[4] < $result_row[5]){ # if it doesn't exceed max
                     if(date('Y-m-d') <= $result_row[3]){ # checks if the class date has passed
                         echo"$result_row[1] <br>";
-                        
-                 }           
+
+                        echo "<input type='checkbox' name='classes[]' value='$result_row[0]'/> $result_row[1] <br />";   
+                    }           
                 }
 
-         }
-    }
+            }
+            echo "</form>";
+        }
     }
 ?>
