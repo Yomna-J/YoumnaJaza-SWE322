b55d7fd6152f40bc415e6d8c03a539d1a7cb4949
Lamia-az <78917527+Lamia-az@users.noreply.github.com>
2021-04-28 01:36:44 +0300

    updated home.php
    created action for confirm

--- Project/www/Home.php
+++ Project/www/Home.php
@@ -16,12 +16,15 @@
         if(mysqli_num_rows($result) > 0){ 
             $result = mysqli_query($connection, $query);
             echo "<form action='' method='post'>";
+            $num_all_trainees = array();
             while ($result_row = mysqli_fetch_row($result)) { # Loop over all classes
                 if($result_row[4] < $result_row[5]){ # if it doesn't exceed max
                     if(date('Y-m-d') <= $result_row[3]){ # checks if the class date has passed
                         echo"$result_row[1] <br>";
 
-                        echo "<input type='checkbox' name='classes[]' value='$result_row[0]'/> $result_row[1] <br />";   
+                        echo "<input type='checkbox' name='classes[]' 
+                        value='$result_row[0]'/> $result_row[1] <br />";
+                        $num_all_trainees += array($result_row[0], $result_row[4]);// array of all classes id , number of trainees 
                     }           
                 }
 
@@ -29,5 +32,29 @@
             echo "<input type='submit' name='confirm' value='Confirm' />
                   </form>";
         }
+    }
+    if ( isset($_POST['confirm'])){
+    $classes = $_POST['classes'];// id{1,2,3,4}
+    if(empty($classes)) 
+    {
+   echo("You didn't select any classes.");
+    } 
+   else 
+  {
+    $num_classes = count($classes);
+    
+   for($i=0; $i < $num_classes; $i++)
+    {
+    $new_num_trainees = $num_all_trainees[$classes[$i]];
+      $query = 'UPDATE classes SET number_of_trainees = $new_num_trainees WHERE class_id =$classes[$i]';
+       $result= mysqli_query($connection, $query);  
+
+    if (!$result = mysqli_query($connection, $query)){
+        die ('An error occured');
+    }
+       
+    }
+  }
+    
     }
 ?>
